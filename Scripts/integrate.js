542e17b6221bf38d2bf7ad5ceea2e06d512dd8b1
jobinpjoseph707 <jobinpjoseph700@gmail.com>
2024-06-03 03:24:01 +0530

    final phase1

---
+++ Scripts/integrate.js
@@ -0,0 +1,22 @@
+// fetch('./html/knockout-frontend.html')
+//             .then(response => response.text())
+//             .then(data => document.getElementById('knockout-nevin').innerHTML = data);
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Load the content of abh section from a.html
+    fetch("knockout-frontend.html")
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, "text/html");
+        const sectionToExport = doc.querySelector("#sectionToExport");
+        const knockoutNevin = document.getElementById("knockout-nevin");
+        
+        // Apply 50% scale to the section
+        // sectionToExport.style.transform = "scale(1)";
+        
+        knockoutNevin.innerHTML = sectionToExport.outerHTML;
+        console.log(sectionToExport.innerHTML);
+      })
+      .catch(error => console.error(error));
+  });
